5f5e35d7e52bab31a4448572222ad7d03afebfdd
Priyanshu Pandey <priyanshun14314@gmail.com>
2026-03-17 20:48:43 +0530

    Started Linked List practice

---
+++ Recursion/TilingProblem.java
@@ -0,0 +1,18 @@
+public class TilingProblem {
+    public static int tiling(int n){    //2xn
+        //Base case
+        if(n==0||n==1){
+            return 1;
+        }
+        //vertical choice
+        int fnm1 = tiling(n-1);
+        //horizontal choice
+        int fnm2 = tiling(n-2);
+        int totalways = fnm1+fnm2;
+        return totalways;
+    }
+    public static void main(String[] args) {
+        System.out.println(tiling(4));
+
+    }
+}
